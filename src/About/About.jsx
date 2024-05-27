65bb3c7c7811e40175cd23e82b4f4a06e2c419c0
jiya-singhal <jiyasinghal95@gmail.com>
2024-05-28 01:39:12 +0530

    initial commit

---
+++ src/About/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+    return (
+        <div class="about-body">
+        <div class = "about-description">
+            <h2>Who Am I</h2>
+            <p>My name's Jiya. I'm a web designer and developer based in Bangalore, India.
+"During the day I work as a lead developer at a local agency and in the evening I work on freelance projects and utilize the time to built my own products. 
+I spent my leisure hours writing articles and poetry. 
+Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas."</p>
+        </div>
+        </div>
+
+    )
+}
+export default About;
---
+++ src/Home/Home.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+
+const Home = () => {
+  return (
+    
+    <div className="home">
+        {/* <img src = "IMG_3517.JPG">jiya</img> */}
+      <div className="signature">
+        <br></br>
+      </div>
+      <div className="launchingPic">
+        <img src="IMG_3517.JPG" alt="Profile Pic" />
+      </div>
+      <div className="designation">
+        Web Developer
+      </div>
+      <div className="Name">
+        <h1>Jiya Singhal</h1>
+      </div>
+      <div className="info">
+      <p>There wasn't a bird in the sky, but that was not what caught her attention. 
+        It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared.</p>
+      </div>
+      <div className="button-container">
+        <button className="download-cv">Download CV</button>
+        <button className="contact">Contact</button>
+      </div>
+      <div className="social-links">
+        <a href="https://www.linkedin.com/in/jiya-singhal-100bbb278/">  
+         
+        </a>
+        <a href="https://github.com/jiya-singhal">  
+         
+        </a>
+        <a href="https://www.instagram.com/jiyasinghal_">  
+          
+        </a>
+        <a href="https://www.youtube.com/@jiyasinghal8368/featured">  
+          
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

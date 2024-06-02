7e865ad381f0630dfa615b0b4cff8d9d8b784aa0
jiya-singhal <155576892+jiya-singhal@users.noreply.github.com>
2024-06-02 22:09:16 +0530

    Update Navbar.js

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -2,19 +2,22 @@ import React from 'react';
 import './Navbar.css';  
 import logo from '../assets/Jiya.png';
 import contactImg from '../assets/contactme.png';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link as RouterLink } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link as ScrollLink } from 'react-scroll'; // Import Link from react-scroll
 
 const Navbar = () => {
   return (
     <nav className="Navbar">
-      <img src={logo} alt="Logo" className="logo" />
+      <RouterLink to="/">
+        <img src={logo} alt="Logo" className="logo" />
+      </RouterLink>
       <div className="desktopMenu">
-        <Link to="/home" className="navbar-menu">Home</Link>
-        <Link to="/about" className="navbar-menu">About</Link>
-        <Link to="/skills" className="navbar-menu">Skills</Link>
-        <Link to="/education" className="navbar-menu">Education</Link>
-        <Link to="/projects" className="navbar-menu">Projects</Link>
-        <Link to="/achievements" className="navbar-menu">Achievements</Link>
+        <ScrollLink to="home" smooth={true} duration={500} className="navbar-menu">Home</ScrollLink>
+        <ScrollLink to="about" smooth={true} duration={500} className="navbar-menu">About</ScrollLink>
+        <ScrollLink to="skills" smooth={true} duration={500} className="navbar-menu">Skills</ScrollLink>
+        <ScrollLink to="education" smooth={true} duration={500} className="navbar-menu">Education</ScrollLink>
+        <ScrollLink to="projects" smooth={true} duration={500} className="navbar-menu">Projects</ScrollLink>
+        <ScrollLink to="achievements" smooth={true} duration={500} className="navbar-menu">Achievements</ScrollLink>
       </div>
       <button className="contactBtn">
         <img src={contactImg} alt="Contact" className="contactImg" />Contact Me
